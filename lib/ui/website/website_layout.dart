76f9a8fdbc506f54bbdb77968a5842e32d71ed4b
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-03-09 22:42:15 +0200

    add fonts to assets

--- lib/ui/website/website_layout.dart
+++ lib/ui/website/website_layout.dart
@@ -8,7 +8,29 @@ class WebsiteLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidgets()
+      appBar: AppBarWidgets(),
+      body: Container(
+        padding: EdgeInsets.all(100),
+        color: Color(0xff0a0a0a),
+        child: Center(
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'Hello My Name is',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 225, 50, 59),
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+              Column(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
