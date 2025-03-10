2f4707055809fceeadbd771db77a50c4612cb2f6
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-03-10 02:26:19 +0200

    edits

--- lib/ui/website/website_layout.dart
+++ lib/ui/website/website_layout.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:personal_website/core/theming/app_text_style.dart';
-import 'package:personal_website/core/theming/colors.dart';
 import 'package:personal_website/ui/website/widgets/about_me_widgets.dart';
 import 'package:personal_website/ui/website/widgets/home_sections.dart';
-import 'package:personal_website/ui/website/widgets/row_icon_widget.dart';
+
 
 import 'widgets/app_bar_widgets.dart';
 
